3d793124e080321c0a5b2215269c4f62dca782d5
YASOOCODE <yaseensaadeldinmohamed@gmail.com>
2022-08-09 11:58:40 +0200

    Fixed JSON Porplems

--- Js/categories.js
+++ Js/categories.js
@@ -8,18 +8,17 @@ fetch("../Json/categriesData.JSON")
         img = document.createElement("img"),
         name = document.createElement("p"),
         next = document.createElement("span");
-      name.innerHTML = ele.title;
-      img.src = ele.image;
+      name.innerHTML = ele.title || "Sample Text.";
+      img.src = ele.image || "images/icon-05.png";
       next.innerHTML = '<i class="fa-solid fa-chevron-right"></i>';
-
       parent.prepend(next);
       parent.prepend(name);
       parent.prepend(img);
       categories.prepend(parent);
     }
   });
-  let container = document.createElement("div");
-  container.classList.add("Slider-Container")
+let container = document.createElement("div");
+container.classList.add("Slider-Container");
 fetch("../Json/categoriesSliderData.JSON")
   .then((response) => response.json())
   .then((data) => {
@@ -45,15 +44,15 @@ fetch("../Json/categoriesSliderData.JSON")
       txtBox.classList.add("text-box");
       cont1.classList.add("cont");
       cont2.classList.add("cont");
-
+      console.log(data);
       // Adding Text
-      img.src = item.imgSrc;
-      namee.innerText = item.title;
+      img.src = item.imgSrc || "images/collection-01.jpg";
+      namee.innerText = item.title || "Genesis Collective Statue";
       colectionP.innerText = "Items in collection:";
       categoryP.innerText = "category:";
-      category.innerText = item.category;
-      colection.innerText = item.colliction;
-      link.innerText = `Explore ${item.title.split(" ")[0]}`;
+      category.innerText = item.category || "Music Art";
+      colection.innerText = item.colliction || "380/394";
+      link.innerText = `Explore ${item.title.split(" ")[0] ||""}`;
 
       // Pending Elements
       cont1.append(colectionP);
@@ -68,110 +67,109 @@ fetch("../Json/categoriesSliderData.JSON")
       slide.append(img);
       txtBox.append(link);
       slide.append(txtBox);
-      container.append(slide)
+      container.append(slide);
       slider.prepend(container);
     });
   });
 
 setTimeout(() => {
   let slideContainer = document.querySelector(".carousel-slider"),
-  slide = document.querySelector(".carousel-slider > div"),
-  slides = document.querySelectorAll(".carousel-slider div .item ")
+    slide = document.querySelector(".carousel-slider > div"),
+    slides = document.querySelectorAll(".carousel-slider div .item ");
 
-const nextBtn = document.querySelector(".control button:nth-child(2)")
-const prevBtn = document.querySelector(".control button:nth-child(1)")
-const interval = 6000;
-// declar the dynamic variable of DOM 
-// let slides = document.querySelectorAll(".slide")
-let index = 1;
-let slidId;
-//create The Colne of the First and last img in the slider
-const firstClone = slides[0].cloneNode(true)
-const lastClone = slides[slides.length - 1].cloneNode(true)
-const secClone = slides[1].cloneNode(true)
-const lastsecClone = slides[slides.length - 2].cloneNode(true)
-const thirClone = slides[2].cloneNode(true)
-const lastthirClone = slides[slides.length - 3].cloneNode(true)
-// add id to the cloned img to mark them
-firstClone.id = "first-clone"
-lastClone.id = "last-clone"
-// append the cloned img
-slide.append(firstClone);
-slide.prepend(lastClone);
-slide.append(secClone);
-slide.prepend(lastsecClone);
-slide.append(thirClone);
-slide.prepend(lastthirClone);
-// git the width of the slide
-let = itemsWidth = slides[index].clientWidth,
-      elementMargin =
+  const nextBtn = document.querySelector(".control button:nth-child(2)");
+  const prevBtn = document.querySelector(".control button:nth-child(1)");
+  const interval = 6000;
+  // declar the dynamic variable of DOM
+  // let slides = document.querySelectorAll(".slide")
+  let index = 1;
+  let slidId;
+  //create The Colne of the First and last img in the slider
+  const firstClone = slides[0].cloneNode(true);
+  const lastClone = slides[slides.length - 1].cloneNode(true);
+  const secClone = slides[1].cloneNode(true);
+  const lastsecClone = slides[slides.length - 2].cloneNode(true);
+  const thirClone = slides[2].cloneNode(true);
+  const lastthirClone = slides[slides.length - 3].cloneNode(true);
+  // add id to the cloned img to mark them
+  firstClone.id = "first-clone";
+  lastClone.id = "last-clone";
+  // append the cloned img
+  slide.append(firstClone);
+  slide.prepend(lastClone);
+  slide.append(secClone);
+  slide.prepend(lastsecClone);
+  slide.append(thirClone);
+  slide.prepend(lastthirClone);
+  // git the width of the slide
+  (let = itemsWidth = slides[index].clientWidth),
+    (elementMargin =
       parseInt(
         window
           .getComputedStyle(
             document.querySelector(".carousel-slider .item:first-child ")
           )
           .margin.split("px")[1]
-      ) * 2
-const slideWidth = itemsWidth + elementMargin;
-// move the slide to left by using the index * width of slide 
-slide.style.transform = `translateX(${-slideWidth * index}px)`
-// function to start sliding
-const startSlide = () => {
+      ) * 2);
+  const slideWidth = itemsWidth + elementMargin;
+  // move the slide to left by using the index * width of slide
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  // function to start sliding
+  const startSlide = () => {
     slidId = setInterval(() => {
-    slides = document.querySelectorAll(".carousel-slider div .item ")
-    if(index >= slides.length - 1) return;
-        index++;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`
-        slide.style.transition = "0.3s"
-    },interval)
-}
-//to make sure that the slide finish to repeat
-slide.addEventListener("transitionend",() => {
-    // To make sure that the append of the cloned img is happend 
-    slides = document.querySelectorAll(".slide")
-    if(slides[index].getAttribute("id") === firstClone.id){
-        slide.style.transition = "none"
-        index = 1;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`
-    }else if(slides[index].getAttribute("id") === lastClone.id){
-        slide.style.transition = "none"
-        index = 4;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`
+      slides = document.querySelectorAll(".carousel-slider div .item ");
+      if (index >= slides.length - 1) return;
+      index++;
+      slide.style.transform = `translateX(${-slideWidth * index}px)`;
+      slide.style.transition = "0.3s";
+    }, interval);
+  };
+  //to make sure that the slide finish to repeat
+  slide.addEventListener("transitionend", () => {
+    // To make sure that the append of the cloned img is happend
+    slides = document.querySelectorAll(".slide");
+    if (slides[index].getAttribute("id") === firstClone.id) {
+      slide.style.transition = "none";
+      index = 1;
+      slide.style.transform = `translateX(${-slideWidth * index}px)`;
+    } else if (slides[index].getAttribute("id") === lastClone.id) {
+      slide.style.transition = "none";
+      index = 4;
+      slide.style.transform = `translateX(${-slideWidth * index}px)`;
     }
-})
-// make a clear of the interval in mouse in
-slideContainer.addEventListener("mouseenter",() => {
-    clearInterval(slidId)
-})
-// call the start slide function when mouse out 
-slideContainer.addEventListener("mouseleave",startSlide);
-// git the next img 
-nextBtn.onclick = () => {
+  });
+  // make a clear of the interval in mouse in
+  slideContainer.addEventListener("mouseenter", () => {
+    clearInterval(slidId);
+  });
+  // call the start slide function when mouse out
+  slideContainer.addEventListener("mouseleave", startSlide);
+  // git the next img
+  nextBtn.onclick = () => {
     // To prevent an error when pressing quickly
-    slides = document.querySelectorAll(".slide")
-    if(index >= slides.length - 1) return;
-
+    slides = document.querySelectorAll(".slide");
+    if (index >= slides.length - 1) return;
 
     index++;
-    slide.style.transition = "0.3s"
-    slide.style.transform = `translateX(${-slideWidth * index}px)`
-}
-// git the previous img
-prevBtn.onclick = () => {
+    slide.style.transition = "0.3s";
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  };
+  // git the previous img
+  prevBtn.onclick = () => {
     // To prevent an error when pressing quickly
-    if(index <= 0) return;
+    if (index <= 0) return;
 
     index--;
-    slide.style.transition = "0.3s"
-    slide.style.transform = `translateX(${-slideWidth * index}px)`
-}
-nextBtn.addEventListener("mouseenter",() => {
-    clearInterval(slidId)
-})
-prevBtn.addEventListener("mouseenter",() => {
-    clearInterval(slidId)
-})
-nextBtn.addEventListener("mouseleave",startSlide)
-prevBtn.addEventListener("mouseleave",startSlide)
-startSlide()
-},3000)
+    slide.style.transition = "0.3s";
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  };
+  nextBtn.addEventListener("mouseenter", () => {
+    clearInterval(slidId);
+  });
+  prevBtn.addEventListener("mouseenter", () => {
+    clearInterval(slidId);
+  });
+  nextBtn.addEventListener("mouseleave", startSlide);
+  prevBtn.addEventListener("mouseleave", startSlide);
+  startSlide();
+}, 3000);
